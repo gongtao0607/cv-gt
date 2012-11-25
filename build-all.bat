186aea3443e75419fb7569ea67e6d6336f0ecaf9
Gong Tao <gongtao0607@gmail.com>
2012-11-25 13:47:38 +0800

    rewrite script to generate different version

--- build-all.bat
+++ build-all.bat
@@ -1,17 +1,1 @@
-move bilingual.tex bilingual.tex.back
-move moderncv.pdf moderncv.pdf.back
-
-echo \cventrue > bilingual.tex
-make
-move moderncv.pdf gt-cv-en.pdf
-
-echo \cvcntrue > bilingual.tex
-make
-move moderncv.pdf gt-cv-cn.pdf
-
-echo \cventrue >> bilingual.tex
-make
-move moderncv.pdf gt-cv-bi.pdf
-
-move bilingual.tex.back bilingual.tex
-move moderncv.pdf.back moderncv.pdf
+bash build-all.sh
---
+++ build-all.sh
@@ -0,0 +1,9 @@
+mv multiout.tex multiout.tex.backup
+mv moderncv.pdf moderncv.pdf.backup
+for file in `ls multiout.*.tex`;do
+	cp $file multiout.tex
+	make
+	mv moderncv.pdf gt-cv-`echo -n $file|cut -d. -f2`.pdf
+done
+mv moderncv.pdf.backup moderncv.pdf
+mv multiout.tex.backup multiout.tex
