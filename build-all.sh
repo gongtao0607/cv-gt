ce67d48f0706c0f096b34a9b27000197d8bd5b3b
Gong Tao <gongtao0607@gmail.com>
2012-12-28 05:42:51 +0800

    changed multi-out many fixes

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,7 @@ mv moderncv.pdf moderncv.pdf.backup
 for file in `ls multiout.*.tex`;do
 	cp $file multiout.tex
 	make
-	mv moderncv.pdf gt-cv-`echo -n $file|cut -d. -f2`.pdf
+	mv moderncv.pdf `echo -n $file|cut -d. -f2`.pdf
 done
 mv moderncv.pdf.backup moderncv.pdf
 mv multiout.tex.backup multiout.tex
